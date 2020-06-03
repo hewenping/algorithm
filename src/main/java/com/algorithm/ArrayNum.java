7663ee9279963c221c7c83626a2c8b30c212aae5
1250435567@qq.com <1250435567@qq.com>
2020-06-03 14:23:56 +0800

    算法

--- src/main/java/com/algorithm/ArrayNum.java
+++ src/main/java/com/algorithm/ArrayNum.java
@@ -8,18 +8,10 @@ public class ArrayNum {
     public static void main(String[] args) {
         Integer []a = new Integer[]{1,8,10,3,9,4,7,11,12,16};
         Integer num=9;
-        Arrays.sort(a);
-        int z=0;
-        //将数组倒序  排除大于9的对象
-        for(int i=0;i<a.length;i++){
-            if(a[i]>=num){
-                z=i;
-            }
-        }
         int l=0;
         int k=0;
-        for(int i=0;i<z;i++){
-            for(int j=0;j<z;j++){
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
                 if(a[i]+a[j]==num){
                     l=i;
                     k=j;
@@ -31,7 +23,7 @@ public class ArrayNum {
         }
 
        //最优算法
-        //先用结果值减去 数组值 判断map值里该元素是否存在  若不存在则存入map中 若存在则将拿出对应的value值即为索引 和数组中对象的索引
+        //先用结果值减去 数组值 判断map值里该元素是否存在  若不存在则存入map中 若存在则将拿出对应的value值即为索引 和数组中对象的索引  典型的空间换时间
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<a.length;i++){
             if(map.containsKey(num-a[i])){
---
+++ src/main/java/com/algorithm/NumOrder.java
@@ -0,0 +1,23 @@
+package com.algorithm;
+
+public class NumOrder {
+    public static void main(String[] args) {
+        int x=114155840;
+        int res = 0;
+        while(x!=0) {
+            //每次取末尾数字
+            int tmp = x%10;
+            //判断是否 大于 最大32位整数
+            if (res>214748364 || (res==214748364 && tmp>7)) {
+                res=0;
+            }
+            //判断是否 小于 最小32位整数
+            if (res<-214748364 || (res==-214748364 && tmp<-8)) {
+                res=0;
+            }
+            res = res*10 + tmp;
+            x /= 10;
+        }
+        System.err.println(res);
+    }
+}
